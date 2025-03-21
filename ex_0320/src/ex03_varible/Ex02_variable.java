810a9cc023dc2d88a9ca56ae2bb00fda095e9add
Sanugie99 <Sanugie99@gmail.com>
2025-03-21 13:12:53 +0900

    Initial commit

---
+++ ex_0320/src/ex03_varible/Ex02_variable.java
@@ -0,0 +1,13 @@
+package ex03_varible;
+
+public class Ex02_variable {
+
+	public static void main(String[] args) {
+		//변수의 유효범위(scope)
+		//모든 변수는 만들어진 중괄호 안에서만 사용할 수 있다.
+		//밖에서 안으로 함수는 적용가능, 반대로 안에서 밖으로 함수 적용은 불가능.
+		String favoriteFood;
+		favoriteFood = "제육";
+	}
+
+}
---
+++ ex_0320/src/ex06_operator/Ex03_operator.java
@@ -0,0 +1,51 @@
+package ex06_operator;
+
+public class Ex03_operator {
+	public static void main(String[] args) {
+		//비트연산자
+		//논리 연산자와 유사하지만 bit단위의 연산만 가능하다.
+		//& 논리곱(and) : 두 항이 모두 참이면 참 아니면 거짓
+		//| 논리합(or) : 두 항 중 하나라도 참이면 참 아니면 거짓
+		//^ 배타적논리합(xor) : 두 항이 다르면 참 같으면 거짓
+		//~ 부정(not) : 참을 거짓으로 거짓을 참으로 연산
+		
+		int a = 10; //1010
+		int b = 7; //0111
+		int c = a & b;
+		System.out.println("c : " + c); //0010 = 2;
+		
+		int a2 = 12; //1100
+		int b2 = 8; //1000
+		int c2 = a2 | b2;
+		System.out.println("c2 : " + c2); //1100 = 12;
+		
+		int a3 = 9; //1001
+		int b3 = 11; //1011
+		int c3 = a3 ^ b3;
+		System.out.println("c3 : " + c3); //0010 = 2;
+		
+		// ~(not)
+		
+		int x = 7; //0111
+		System.out.println("~x : " + ~x); //~가 앞에 있으므로 1과 0 이 바뀜 0111 -> 1000
+		//~x -> -(x+1)
+		
+		//시프트연산자
+		//비트의 이동
+		//<<, >>
+		
+		x = 10; //1010
+		int result = x << 1;//왼쪽으로 한칸씩 이동
+		System.out.println(result);//1010(0) = 20
+		result = x >> 1;//오른쪽으로 한칸씩 이동
+		System.out.println(result);//(0)0101 = 5
+		
+		//삼항연산자
+		//조건식? 참일 때 실행할 명령 : 거짓일때 실행할 명령;
+		
+		int i = 10;
+		int i2 = 15;
+		result = ++i >= i2 ? 1 : 0;
+		System.out.println("result : " + result);
+	}
+}
