c204277308c4fd1d08b33d159026f366bb1754e2
Sanugie99 <Sanugie99@gmail.com>
2025-04-16 18:11:44 +0900

    2025.04.16

---
+++ ex_0416/src/Exam/StudentMain.java
@@ -0,0 +1,16 @@
+package Exam;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class StudentMain {
+	public static void main(String[] args) {
+		ArrayList<String> Students = new ArrayList<>();
+		
+		Student s;
+		
+		
+		
+	}
+}
